bd189fa3a3eae84e89a09d24ce39a94a9d733fdb
vaibhavguptacodes-collab <vaibhavgupta.codes@gmail.com>
2026-05-09 11:04:08 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+
+        int prev1 = 2;
+        int prev2 = 1;
+
+        int curr = 0;
+
+        for(int i=3;i<=n;i++){
+            curr = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+}
